44dc844de6a6e6283264df939b9be29d5dbf0e2d
jobcito <jorge.orellana.cl@outlook.com>
2021-09-01 09:24:04 -0400

    Actualización 30-08-2021

--- Uautonoma/Optativo_III_2_2021/semana3/TestDocker_30_08_2021/app1/app.py
+++ Uautonoma/Optativo_III_2_2021/semana3/TestDocker_30_08_2021/app1/app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     print('************* DESDE APP1')
-    return '<h1>Hola desde Flask que esta en APP1 pero APP2 la consulta y la expone en la Web</h1>'
+    return '<h5>Hola desde Flask que esta en APP1 pero APP2 la consulta y la expone en la Web</h5>'
