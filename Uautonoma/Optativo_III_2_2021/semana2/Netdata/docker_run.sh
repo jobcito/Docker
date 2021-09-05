163507139ba1a075097dd7fb5582828422dc7b9a
jobcito <jorge.orellana.cl@outlook.com>
2021-09-05 20:38:22 -0300

    Actualizacion 05-09-2021

--- Uautonoma/Optativo_III_2_2021/semana2/Netdata/docker_run.sh
+++ Uautonoma/Optativo_III_2_2021/semana2/Netdata/docker_run.sh
@@ -13,8 +13,8 @@ docker run -d --name=netdata \
   --restart unless-stopped \
   --cap-add SYS_PTRACE \
   --security-opt apparmor=unconfined \
-  -e NETDATA_CLAIM_TOKEN=QPA2AycoiVCOHGwOkRrqo1xOsY5Lv4wnrRWrgHEvw7w1MVEDy8UtC8DZl8RKdlDh_Dkr6RiOs52oqXRv9easEjDbeu3BD8ihW1UvTz-dtLBfN0DWd-veMbG44tVtuLSGbKtb85w \
+  -e NETDATA_CLAIM_TOKEN=sZpekOiXleRNDpM-8Stb--14yxA1qPnEQAR_RO3Z2676DiXFaYvoRU3lpyk3Fevt0WkciNCbNH6n21recMrcxrbS-_hU0QNCC3FEbyMY21DIv-X-nPs0Lmith50shp_Ilym_rBs \
   -e NETDATA_CLAIM_URL=https://app.netdata.cloud \
-  -e NETDATA_CLAIM_ROOMS=1905ecb6-1765-4ea4-b254-101e95016747 \
+  -e NETDATA_CLAIM_ROOMS=1a461069-390d-4f57-af99-d8a35aa843ca \
   netdata/netdata \
   /
--- Uautonoma/Optativo_III_2_2021/semana4/app1/project/health/endpoints.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-
-health_blueprint = Blueprint('health', __name__)
-
-
-@health_blueprint.route('/health', methods=['GET'])
-def health():
-    return 'OK', 200
