e4e55790062383f3b9e1f146921cf4722f6fd398
jobcito <60305158+jobcito@users.noreply.github.com>
2021-09-05 20:14:15 -0300

    Delete Uautonoma/Optativo_III_2_2021/semana4 directory

--- Uautonoma/Optativo_III_2_2021/semana4/app1/project/users/models.py
+++
@@ -1,7 +0,0 @@
-from project import db
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String)
